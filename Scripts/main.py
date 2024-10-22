546ee2e7a5e8dc71b4f045830cfa11281d89be3b
cristobalstrange <cristobal.strange@uc.cl>
2024-10-22 20:47:59 -0300

    No funciona

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import numpy as np
+from time import sleep
 
 df = pd.DataFrame(columns=["habitaciones","baños","a_construida","a_total","estacionamientos","precio", "comuna"])
-
+df = None
 # URL de la página a scrapeear
 url = "https://chilepropiedades.cl/propiedades/venta/casa/region-metropolitana-de-santiago-rm/0"
 
@@ -11,28 +13,36 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 # Realiza la solicitud HTTP a la página
 response = requests.get(url, headers=headers)
 # Verifica que la solicitud fue exitosa (código 200)
-for i in range(1109):
-    if response.status_code != 200:
-        0/0
+for i in range(29,61):
     # Parsear el contenido HTML de la página
     url = f"https://chilepropiedades.cl/propiedades/venta/casa/region-metropolitana-de-santiago-rm/{i}"
+    print(url)
     response = requests.get(url, headers=headers)
-
+    print("Response: ",response)
     soup = BeautifulSoup(response.content, 'html.parser')
-
+    print(soup)
     # Encuentra todos los elementos de tipo título de artículos (suponiendo que están en etiquetas <h2>)
     titles = soup.find_all('div', {"class": "clp-premium-table-item"})
     # Imprime cada título
     # print(titles[0])
     tipos={"1":1,"2":949,"3":37960} # CLP, USD, UF
+
     for titulo in titles:
-        fila = []
+        fila = {"Estacionamientos:": np.nan, "Baños:":np.nan, "Superficie Construida:":np.nan, "Estacionamientos:": np.nan,"valor" : np.nan, "comuna":np.nan}
         for a in titulo.find_all('span',{"class":"clp-feature-value"}):
-            # print(a.parent.find_all('span',{"class": "clp-feature-description"})[0].text, a.text)
+            if len(a.parent.find_all('span',{"class": "clp-feature-description"})) != 0:
+                label = a.parent.find_all('span',{"class": "clp-feature-description"})[0].text
+            print(label, a.text)
             if "m" in a.text:
-                fila.append(float(a.text[:-2].replace(",",".")))
+                try:
+                    if fila[label] != np.nan:
+                        label = label+"_2"
+                    fila[label]=(float(a.text[:-2].replace(",",".")))
+                except:
+                    fila[label]=np.nan
             else:
-                fila.append(a.text)
+                fila[label]=(a.text)
+
 
         tipo = (titulo.find_all('span', {"class":"clp-value-container"}))[1]["valueunit"]
         valor = titulo.find_all('span',{"class":"clp-value-container"})[1]["value"]
@@ -40,19 +50,16 @@ for i in range(1109):
             valor = float(valor.split("E")[0])*10**float(valor.split("E")[1])
         else:
             valor = float (valor)
-        fila.append(valor*tipos[tipo])
+        fila["valor"] = (valor*tipos[tipo])
 
 
-        fila.append((titulo.find_all('h3',{"class":"sub-codigo-data"}))[0].text.split("/")[2].strip())
-        if len(fila) == 6:
-            fila.append("nan")
-        elif len(fila) <6:
-            print(fila)
-            0/0
+        fila["comuna"] = ((titulo.find_all('h3',{"class":"sub-codigo-data"}))[0].text.split("/")[2].strip())
+
         print(fila)
-        df = pd.concat([pd.DataFrame([fila], columns=df.columns), df], ignore_index=True)
+        if df is None:
+            df = pd.DataFrame(columns=fila.keys())
+        df = pd.concat([pd.DataFrame(fila, index=[0]),df], ignore_index=True)
         print(df)
-else:
-    print(f"Error al acceder a la página, código de estado: {response.status_code}")
+        #sleep(.5)
 
-df.to_csv("datos.csv",index=False)
+df.to_csv("datos2.csv",index=False)
